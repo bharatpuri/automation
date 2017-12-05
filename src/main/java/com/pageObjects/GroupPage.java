7569efdc360a3081f9d1dab2a73347de381cf025
bharatpuri <puribharta48@gmail.com>
2017-12-05 18:43:04 +0530

    remove crendentails class

--- src/main/java/com/pageObjects/GroupPage.java
+++ src/main/java/com/pageObjects/GroupPage.java
@@ -1,5 +1,37 @@
 package com.pageObjects;
 
+import com.selenium.common.Locator;
+import com.selenium.common.Locator.LocatorType;
+
+/**
+ * This class will contains all the elements of Groups page.
+ * @author bharatp
+ *
+ */
 public class GroupPage {
+	public static Locator groupTab = new Locator(LocatorType.ID, "lrsadminAccGroupItem");
+
+	public static Locator addNewGroup = new Locator(LocatorType.ID, "addAccountGroups");
+
+	public static Locator viewAllGroups = new Locator(LocatorType.ID, "showAllAccountGroup");
+
+	public static Locator accountGroupName = new Locator(LocatorType.ID, "accountGroupName");
+
+	public static Locator groupDescription = new Locator(LocatorType.ID, "description");
+
+	public static Locator cancelGroupButton = new Locator(LocatorType.XPATH, "//button[contains(.,'Cancel')]");
+
+	public static Locator saveGroupButton = new Locator(LocatorType.XPATH, "//button[contains(.,'Save')]");
+
+	public static Locator deleteGroupButton = new Locator(LocatorType.XPATH, "//button[contains(.,'Delete Group')]");
+
+	public static Locator firstGroupFromList = new Locator(LocatorType.XPATH, "//div[(@class='mdMasterNode')]");
+
+	// /**
+	// * Constructor will initialized all the elements of this class
+	// */
+	// public GroupPage() {
+	// PageFactory.initElements(Browser.getDriver(), this);
+	// }
 
 }
--- src/test/java/com/automation/testCases/LoginTestDemo.java
+++ src/test/java/com/automation/testCases/LoginTestDemo.java
@@ -1,25 +1,10 @@
 package com.automation.testCases;
 
-import org.testng.annotations.Test;
-
-import com.automation.base.CustomException;
 import com.automation.browser.Browser;
 import com.automation.browser.Chrome;
 
-import applicationActions.LoginPageFunctionality;
-
 public class LoginTestDemo {
-	LoginPageFunctionality login;
+	
 	Browser browser = new Chrome();
 
-	public LoginTestDemo() throws CustomException {
-		login = new LoginPageFunctionality();
-	}
-
-	@Test
-	public void login() throws CustomException {
-
-		browser.openBrowser();
-		login.loginAsSuperUser();
-	}
 }
